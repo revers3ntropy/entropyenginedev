f286407fd4c52e84f50902e576682be2bbe8104d
revers3ntropy <josephcoppin@gmail.com>
2021-06-23 23:19:51 +0100

    ignored .js files in entropy-engine directory, try 7

--- entropy-engine/src/render/3d/drawTriangle.ts
+++
@@ -1,8 +0,0 @@
-import {TriangleV3} from "../../util/maths/maths3D.js";
-import {Sprite} from "../../ECS/sprite.js";
-import * as draw from "../renderer.js";
-
-export function wireframe (triangle: TriangleV3, ctx: CanvasRenderingContext2D, camera: Sprite) {
-    const tri = triangle.clone.project3D(ctx, camera);
-    draw.polygon(ctx, tri.triangleV2.points, 'rgb(0, 0, 0)', false);
-}
